97ef03e7c2bc554fa7f6e7a59d8ca131dbe1e6e1
Donald Derek <donaldderek.haddad@gmail.com>
2014-08-28 15:51:51 +0300

    added gmap

--- config/express.js
+++ config/express.js
@@ -4,7 +4,7 @@ module.exports = function (app, config) {
     app.set('views', config.root + '/views');
     app.set('view engine', 'hjs');
     app.use(express.compress());
-    app.use('/static', express.static(config.root + '/public'));
+    app.use('/', express.static(config.root + '/public'));
     app.set('port', process.env.PORT || 3000);
     app.use(express.favicon());
     app.use(express.logger('dev'));
