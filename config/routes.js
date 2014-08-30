39c321fbd96c5174a3540193d5253dcb7b0b2912
Donald Derek <donaldderek.haddad@gmail.com>
2014-08-30 14:32:29 +0300

    routes

--- config/routes.js
+++ config/routes.js
@@ -8,7 +8,7 @@ module.exports = function(app){
     app.get('/api/spaces',spaces.readAll);
     app.get('/api/spaces/search/',spaces.search);
     app.get('/api/spaces/:id',spaces.singleRead);
-    app.get('/spaces/:uuid',spaces.singleReadLongURI);
+    app.get('/:uuid',spaces.singleReadLongURI);
     app.put('/api/spaces/:id', spaces.update);
     app.delete('/api/spaces/:id',spaces.delete);
 
