e0b8a52b0c2f3ab6fdf7fbf8e0f26c3f1120e387
Donald Derek <donaldderek.haddad@gmail.com>
2014-08-31 23:36:26 +0300

    remove :id

--- config/routes.js
+++ config/routes.js
@@ -4,10 +4,10 @@ module.exports = function(app){
     app.get('/',function(req, res){
         return res.redirect("http://www.peerspace.com/4.04.4004");
     });
-    app.post('/api/spaces',spaces.create);
+    //app.post('/api/spaces',spaces.create);
     app.get('/api/spaces',spaces.readAll);
     app.get('/api/spaces/search/',spaces.search);
-    app.get('/api/spaces/:id',spaces.singleRead);
+    //app.get('/api/spaces/:id',spaces.singleRead);
     app.get('/:uuid',spaces.singleReadLongURI);
     app.put('/api/spaces/:id', spaces.update);
     app.delete('/api/spaces/:id',spaces.delete);
