617983ea155407f3efa2b739dc008621d9243da3
Donald Derek <donaldderek.haddad@gmail.com>
2014-10-20 20:39:41 -0400

    fixed price

--- public/js/dist/ps-listing.js
+++ public/js/dist/ps-listing.js
@@ -1164,7 +1164,25 @@ if($(window).width() <= 770) {
     }
   };
 })(jQuery);
-;jQuery(document).ready(function($){
+;Number.prototype.formatMoney = function(c, d, t){
+var n = this,
+    c = isNaN(c = Math.abs(c)) ? 2 : c,
+    d = d == undefined ? "." : d,
+    t = t == undefined ? "," : t,
+    s = n < 0 ? "-" : "",
+    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+jQuery(document).ready(function($){
+
+    //Format Currency
+    var ammount = $('.list-info li').first();
+    var frmAmmount = ammount.text().split('$')[1];
+    frmAmmount = frmAmmount/100;
+    console.log(frmAmmount);
+    ammount.html('<strong>Price Per Hour: $</strong>'+frmAmmount.formatMoney(2, '.', ','));
 
     //Stars
     var stars = $(".prf-raiting").attr('id');
